87940a254f267d3fa3de9803ac03f250acfa8ff5
arkgrowth14 <arkgrowth14@gmail.com>
2026-03-11 22:53:31 -0400

    Update index.ts

--- integrations/index.ts
+++ integrations/index.ts
@@ -1,2 +1,10 @@
-export * from './cms';
+export class BaseCrudService<T> {
+async list() { return { items: [] as T[] }; }
+async get() { return null as T | null; }
+async create(data: any) { return data as T; }
+async update(id: string, data: any) { return data as T; }
+async delete() { return true; }
+}
+
 export * from './members';
+export * from './cms';
